507dddf917294f398baa19bf3d6578a1392b51d8
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2023-09-29 11:38:32 +0200

    delete test.js

--- js/test.js
+++
@@ -1,8 +0,0 @@
-importScripts("https://via.batch.com/v3/worker.min.js");
-
-const eventsList = ["pushsubscriptionchange", "install", "push", "notificationclick", "message"];
-eventsList.forEach(eventName => {
-  self.addEventListener(eventName, event => {
-    event.waitUntil(self.handleBatchSDKEvent(eventName, event));
-  });
-});
